94c07b7ba4945243e55a045dbea218a605a95c92
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-09 13:19:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+//개발 순서 큰그림 정리
+//1. 앱이 실행되자 마자 현재 위치 기반 날씨를 보여준다.
+//2. 날씨정보에는 도시, 섭/화씨 날씨 상태
+//3. 5개의 버튼이 있다.(현재위치, 4개의 다른 도시들)
+//4. 도시 버튼을 누를 때마다 해당 도시의 날씨정보가 보임.
+//5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨정보 보여줌
+//6. 데이터 로딩 중일때 로딩스피너 구현
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Welcome to Weather App!
     </div>
   );
 }
