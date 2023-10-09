2f41a10b3c02e2cf884b2725beb808653b91af44
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-09 16:30:42 +0200

    get weather information by API Call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState, useEffect} from 'react';
 //개발 순서 큰그림 정리
 //1. 앱이 실행되자 마자 현재 위치 기반 날씨를 보여준다.
+// ->useEffect()가 필요하다.
+//현재 위치는 언제 가져오나 ? 
 //2. 날씨정보에는 도시, 섭/화씨 날씨 상태
 //3. 5개의 버튼이 있다.(현재위치, 4개의 다른 도시들)
 //4. 도시 버튼을 누를 때마다 해당 도시의 날씨정보가 보임.
@@ -12,6 +14,27 @@ import './App.css';
 
 
 function App() {
+
+  const getCurrentLocation = () =>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      getWeatherByCurrentLocation(lat,lon)
+    })
+  }
+  
+  const getWeatherByCurrentLocation = async (lat,lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e59c97f8ad9f843a63a40588ff11b1df`
+    let response = await fetch(url)
+    //윗줄이 비동기.
+    let data = await response.json();
+    console.log("data",data);
+  }
+  
+  useEffect(()=>{
+    getCurrentLocation();
+  },[])
+
   return (
     <div>
       Welcome to Weather App!
